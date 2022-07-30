b35a041eb4fdf839dcf7fbfa8d786a33d7e8a62d
martamazurkiewicz <marta@mazurkiewi.cz>
2022-07-30 23:31:14 +0200

    Moved app screen to different folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tabata_timer/main.dart';
+import 'package:tabata_timer/screens/app_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const AppScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
